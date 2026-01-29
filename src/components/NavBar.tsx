5420c66443f41b81f1d24a27133d8eafd6b597e4
Jhordan Fabros <fabrosjhordan@gmail.com>
2026-01-29 22:11:02 +0800

    Update NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -14,7 +14,7 @@ export const NavBar = () =>{
 
     useEffect(()=>{
         const handleScroll = () =>{
-            setIsScrolled(window.screenY > 10);
+            setIsScrolled(window.scrollY > 10);
         }
         window.addEventListener('scroll', handleScroll);
         return ()=> window.removeEventListener('scroll', handleScroll)
